2cae6458bdc80e4468157efa82e73abe6b81bcca
Greg Bowler <greg.bowler@g105b.com>
2014-04-29 14:04:27 +0100

    Create Pjax.js

---
+++ Pjax.js
@@ -0,0 +1,112 @@
+/**
+ * Pjax: The concept of using ajax (XMLHttpRequest) and pushState to produce
+ * instant-loading links for speeding up sites and apps, without breaking the
+ * browser's back button.
+ *
+ * http://github.com/g105b/pjax
+ */
+;document.addEventListener("DOMContentLoaded", function() {
+var
+	// First find all elements with the data-pjax attribute.
+	pjaxContainerList = document.querySelectorAll("[data-pjax]:not(a)"),
+	pjaxAnchorList = 
+		document.querySelectorAll("a[data-pjax]:not([data-pjax='false'])"),
+	// Then store all links to pre-load in their own array.
+	pjaxAnchorArray = [],
+	len_anchor,
+	i_anchor,
+
+	pjax = {},
+$;
+
+// Add anchors within containers to the array:
+Array.prototype.forEach.call(pjaxContainerList, function(el) {
+	var
+		data = el.getAttribute("data-pjax"),
+		list,
+	$;
+
+	if(data == "true") {
+		data = "a";
+	}
+
+	list = el.querySelectorAll(data);
+
+	Array.prototype.forEach.call(list, function(anchor) {
+		pjaxAnchorArray.push(anchor);
+	});
+});
+
+// Add anchors with their own data-pjax attribute to the array:
+Array.prototype.forEach.call(pjaxAnchorList, function(anchor) {
+	pjaxAnchorArray.push(anchor);
+});
+
+// Load cached page for each anchor's href:
+len_anchor = pjaxAnchorArray.length;
+for(i_anchor = 0; i_anchor < len_anchor; i_anchor++) {
+	cache(pjaxAnchorArray[i_anchor]);
+}
+
+function cache(anchor) {
+	var
+		xhr = new XMLHttpRequest(),
+	$;
+
+	if(localStorage[anchor.pathname]) {
+		if(pjax[anchor.pathname]) {
+			return;
+		}
+
+		cacheLoad(anchor, localStorage[anchor.pathname]);
+		return;
+	}
+
+	xhr.open("get", anchor.pathname, true);
+	xhr.addEventListener("load", function() {
+		cacheLoad(anchor, this.responseText);
+	});
+	xhr.send();
+};
+
+function cacheLoad(anchor, html) {
+	var
+		doc = document.implementation.createHTMLDocument(''),
+		parent = anchor,
+	$;
+
+	doc.documentElement.innerHTML = html;
+	localStorage[anchor.pathname] = html;
+
+	pjax[anchor.pathname] = {
+		"title": doc.title,
+		"body": doc.body.innerHTML,
+	};
+
+	while(parent) {
+		if(parent.hasAttribute("data-pjax-event")) {
+			parent.getAttribute("data-pjax-event").split(" ").forEach(
+			function(eventName) {
+				console.log(eventName);
+				anchor.addEventListener(eventName, e_click);
+			});
+			break;
+		}
+		parent = parent.parentElement;
+	}
+	anchor.addEventListener("click", e_click);
+};
+
+function e_click(e) {
+	if(!pjax[this.pathname]) {
+		return true;
+	}
+
+	e.preventDefault();
+
+	document.title = pjax[this.pathname].title;
+	document.body.innerHTML = pjax[this.pathname].body;
+	document.dispatchEvent(new Event("DOMContentLoaded"));
+};
+
+});
